357a82437824d69ba85e9296e4bc64ebc3a34908
xrwz <32107271+xrwz@users.noreply.github.com>
2018-06-07 11:51:55 +0000

    Update virtual.presence.sensor.groovy

--- devicetypes/virtual.presence.sensor.groovy
+++ devicetypes/virtual.presence.sensor.groovy
@@ -4,7 +4,7 @@
 
 
 metadata {
-        definition (name: "Virtual Presence Plus", namespace: "ajpri", author: "Austin Pritchett") {
+        definition (name: "Virtual Presence Plus", namespace: "xrwz", author: "Austin Pritchett") {
         capability "Switch"
         capability "Refresh"
         capability "Presence Sensor"
